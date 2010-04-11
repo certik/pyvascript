b4af6dda5b45b22c4db3998482f58355fec486e9
Ondrej Certik <ondrej@certik.cz>
2010-04-11 15:47:24 -0700

    convert tabs to spaces

--- examples/inline.py
+++ examples/inline.py
@@ -3,31 +3,31 @@ from AjaxHelper import *
 
 @JavaScript
 def test():
-	alert('Test!')
-	TestClass.new() # Notice the 'new'
+    alert('Test!')
+    TestClass.new() # Notice the 'new'
 
 class TestClass(JavaScript):
-	def __init__(self):
-		alert('TestClass created')
-		self.reset()
-	
-	def reset(self):
-		self.value = 0
-	
-	def inc(self):
-		alert(self.value)
-		self.value += 1
+    def __init__(self):
+        alert('TestClass created')
+        self.reset()
+
+    def reset(self):
+        self.value = 0
+
+    def inc(self):
+        alert(self.value)
+        self.value += 1
 TestClass = TestClass()
 
 class AjaxTest(AjaxHelper):
-	def __init__(self):
-		self.post('/some/url')
-	
-	def success(self, data):
-		alert(data)
-	
-	def failure(self, o):
-		alert('Ajax failure...')
+    def __init__(self):
+        self.post('/some/url')
+
+    def success(self, data):
+        alert(data)
+
+    def failure(self, o):
+        alert('Ajax failure...')
 AjaxTest = AjaxTest()
 
 print '<script language="Javascript">'
