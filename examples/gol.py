97fdd879c376f9f60526042ba656f156a463f24b
Ondrej Certik <ondrej@certik.cz>
2010-04-11 18:50:24 -0700

    Add the whole html stanzas

--- examples/gol.py
+++ examples/gol.py
@@ -67,8 +67,15 @@ class GoL(JavaScript):
 GoL = GoL()
 
 def main():
-    print '<script language="JavaScript">\n%s</script>' % \
-            (str(StartGoL)+str(GoL))
+    print """<html>
+<script language="JavaScript">
+%s
+</script>
+<body onLoad="StartGoL()">
+    <canvas id="canvas" width="750" height="750"></canvas>
+</body>
+</html>""" % (str(StartGoL)+str(GoL))
+
 
 if __name__ == "__main__":
     main()
