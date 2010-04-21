58ff03857330af169c27467ecbd2a7605376808a
Ondrej Certik <ondrej@certik.cz>
2010-04-21 00:56:56 -0700

    Other python tests added

--- tests/test_python.py
+++ tests/test_python.py
@@ -10,6 +10,40 @@ def f3(x):
     a = x + 1
     return a - 5
 
+def f3b(x):
+    a = x + 1
+    a -= 5
+    return a
+
+def f3c(x):
+    a = x + 1
+    a /= 5
+    return a
+
+def f3d(x):
+    a = x + 1
+    a *= 5
+    return a
+
+def f3e(x):
+    a = x + 1
+    a += 5
+    return a
+
+def f4(x):
+    if x:
+        return 5
+    else:
+        return 6
+
+def f5(x):
+    a = 1
+    if x:
+        a = a + 1
+    else:
+        a = a - 1
+    return a
+
 def test(func, arg_array):
     code = compile(str(Python(func)), "", "exec")
     namespace = {}
@@ -22,5 +56,11 @@ def test_basic():
     test(f1, [1, 2, 3, "x"])
     test(f2, [1, 2, 3, -5])
     test(f3, [1, 2, 3, -5])
+    test(f3b, [1, 2, 3, -5])
+    test(f3c, [1, 2, 3, -5])
+    test(f3d, [1, 2, 3, -5])
+    test(f3e, [1, 2, 3, -5])
+    test(f4, [True, False])
+    test(f5, [True, False])
 
 test_basic()
