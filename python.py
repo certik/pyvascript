087f56305cb8ea47b064c3f35b5498cdc5f45b77
Ondrej Certik <ondrej@certik.cz>
2010-04-21 19:03:19 -0700

    Move the code to python.py

--- python.py
+++ python.py
@@ -1,3 +1,135 @@
+from ast import parse, iter_fields, AST, dump
+
+def flatten(l):
+    s = []
+    if isinstance(l, list):
+        for item in l:
+            s.extend(flatten(item))
+    else:
+        s.append(l)
+    return s
+
+
+class NodeVisitor(object):
+
+    def visit(self, node):
+        """Visit a node."""
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        """Called if no explicit visitor function exists for a node."""
+        s = []
+        for field, value in iter_fields(node):
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, AST):
+                        s.append(self.visit(item))
+                    else:
+                        raise NotImplementedError("Some problem.")
+            elif isinstance(value, AST):
+                s.append(self.visit(value))
+            else:
+                # I don't understand why we get here sometimes, but we need to
+                # skip it:
+                pass
+        return node.__class__.__name__ + str(tuple(s))
+
+class PythonVisitor(NodeVisitor):
+
+    def __init__(self):
+        self._scope = []
+
+    def indent_block(self, l):
+        s = []
+        for statement in l:
+            stat = self.visit(statement)
+            if isinstance(stat, str):
+                s.append("    %s" % stat)
+            else:
+                for item in stat:
+                    s.append("    %s" % item)
+        return s
+
+    def visit_If(self, node):
+        s = []
+        s.append("if %s:" % self.visit(node.test))
+        s.extend(self.indent_block(node.body))
+        if len(node.orelse) > 0:
+            s.append("else:")
+            s.extend(self.indent_block(node.orelse))
+        return s
+
+    def visit_Assign(self, node):
+        assert len(node.targets) == 1
+        target = node.targets[0]
+        return ["%s = %s" % (self.visit(target), self.visit(node.value))]
+
+    def visit_AugAssign(self, node):
+        return ["%s %s= %s" % (self.visit(node.target),
+            self.visit(node.op),
+            self.visit(node.value))]
+
+    def visit_Num(self, node):
+        return str(node.n)
+
+    def visit_Gt(self, node):
+        return ">"
+
+    def visit_Add(self, node):
+        return "+"
+
+    def visit_Sub(self, node):
+        return "-"
+
+    def visit_Mult(self, node):
+        return "*"
+
+    def visit_Div(self, node):
+        return "/"
+
+    def visit_arguments(self, node):
+        return [self.visit(arg) for arg in node.args]
+
+    def visit_Return(self, node):
+        return ["return %s" % self.visit(node.value)]
+
+    def visit_FunctionDef(self, node):
+        s = []
+        args = ", ".join(self.visit(node.args))
+        s.append("def %s(%s):" % (node.name, args))
+        s.extend(self.indent_block(node.body))
+        return s
+
+
+    def visit_Compare(self, node):
+        assert len(node.ops) == 1
+        assert len(node.comparators) == 1
+        op = node.ops[0]
+        comp = node.comparators[0]
+        return "%s %s %s" % (self.visit(node.left),
+                self.visit(op),
+                self.visit(comp)
+                )
+
+    def visit_Module(self, node):
+        return flatten([self.visit(s) for s in node.body])
+
+    def visit_Name(self, node):
+        return node.id
+
+    def generic_visit(self, node):
+        print "Generic visit:", node.__class__.__name__, node._fields
+        return super(PythonVisitor, self).generic_visit(node)
+
+def transform_py(s):
+    v = PythonVisitor()
+    t = parse(s)
+    return "\n".join(v.visit(t))
+
+#################
+# this is the obsolete old version:
 import dis, struct
 from translator import Translator, opcode
 
